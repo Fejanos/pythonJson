d653468c8e57f2cd50e1abc3dc07856af9004a5f
Fejanos <fenyveijani97@gmail.com>
2025-09-24 19:12:04 +0200

    Dictionary & JSON

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello, world")
+# key : value
+student = {
+    "name": "Gipsz Jakab",
+    "age": 25,
+    "skills": ["Python", "Linux", "Git"]
+}
+
+print(student["name"]) # Gipsz Jakab
+print(student["age"])
+print(student["skills"])
+
+students = [
+    {
+        "name": "Gipsz Jakab",
+        "age": 25,
+        "skills": ["Python", "Linux", "Git"]
+    },
+    {
+        "name": "Para Zita",
+        "age": 28,
+        "skills": ["C#", "Windows", "Git"]
+    }
+]
+
+for s in students:
+    print(s["name"])
